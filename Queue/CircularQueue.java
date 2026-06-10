a8619ea72211059c57325bfcd30a75ebda0e281a
vinkims <vincekigen@gmail.com>
2026-06-10 23:40:13 +0300

    Add Circular Queue

---
+++ Queue/CircularQueue.java
@@ -0,0 +1,106 @@
+public class CircularQueue {
+    int SIZE = 5;
+    int front, rear;
+    int items[] = new int[SIZE];
+
+    CircularQueue() {
+        front = -1;
+        rear = -1;
+    }
+
+    // Check if queue is full
+    boolean isFull() {
+        if (front == 0 && rear == SIZE - 1) {
+            return true;
+        }
+        if (front == rear + 1) {
+            return true;
+        }
+        return false;
+    }
+
+    // Check if queue is empty
+    boolean isEmpty() {
+        if (front == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    // Adding en element
+    void enQueue(int element) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+        } else {
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % SIZE;
+            items[rear] = element;
+            System.out.println("Inserted " + element);
+        }
+    }
+
+    // Removing an element
+    int deQueue() {
+        int element;
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return (-1);
+        } else {
+            element = items[front];
+            if (front == rear) {
+                front = -1;
+                rear = -1;
+            } else {
+                /* Q has only one element; reset the queue after deleting it. */
+                front = (front + 1) % SIZE;
+            }
+            return (element);
+        }
+    }
+
+    void display() {
+        int i;
+        if (isEmpty()) {
+            System.out.println("Empty queue");
+        } else {
+            System.out.println("Front -> " + front);
+            System.out.println("Items -> ");
+            for (i = front; i != rear; i = (i + 1) % SIZE) {
+                System.out.println(items[i] + " ");
+            }
+            System.out.println(items[i]);
+            System.out.println("Rear -> " + rear);
+        }
+    }
+
+    public static void main(String[] args) {
+        CircularQueue q = new CircularQueue();
+
+        q.deQueue();
+
+        q.enQueue(1);
+        q.enQueue(2);
+        q.enQueue(3);
+        q.enQueue(4);
+        q.enQueue(5);
+
+        q.enQueue(6);
+
+        q.display();
+
+        int elem = q.deQueue();
+
+        if (elem != -1) {
+            System.out.println("Deleted element is " + elem);
+        }
+        q.display();
+
+        q.enQueue(7);
+
+        q.display();
+
+        q.enQueue(8);
+    }
+}
